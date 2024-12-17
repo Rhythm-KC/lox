ca426bab29edee96bef26a5ef5dbf3611c401899
Rhythm-KC <rhythmkc7@gmail.com>
2024-12-17 11:25:58 -0500

    moved AST printer to the right package

--- src/main/java/com/rhythm/lox/Parser/Expr.java
+++ src/main/java/com/rhythm/lox/Parser/Expr.java
@@ -4,16 +4,16 @@ import com.rhythm.lox.Scanner.Token;
 
 public abstract class Expr {
 
-  public abstract <R> R accept(Visitor<R> visitor);
+  abstract <R> R accept(Visitor<R> visitor);
 
-  public interface Visitor<R> {
+  interface Visitor<R> {
     R visitBinaryExpr(Binary expr);
     R visitGroupingExpr(Grouping expr);
     R visitLiteralExpr(Literal expr);
     R visitUnaryExpr(Unary expr);
   }
 
-  public static class Binary extends Expr {
+  static class Binary extends Expr {
     public final Expr left;
     public final Token operator;
     public final Expr right;
@@ -25,40 +25,40 @@ public abstract class Expr {
     }
 
     @Override
-    public <R> R accept(Visitor<R> visitor) {
+    <R> R accept(Visitor<R> visitor) {
       return visitor.visitBinaryExpr(this);
     }
   }
-  public static class Grouping extends Expr {
+  static class Grouping extends Expr {
 
-    public final Expr expression;
+    final Expr expression;
 
     Grouping(Expr expression) {
       this.expression = expression;
     }
 
     @Override
-    public <R> R accept(Visitor<R> visitor) {
+    <R> R accept(Visitor<R> visitor) {
       return visitor.visitGroupingExpr(this);
     }
 
   }
-  public static class Literal extends Expr {
-    public final Object value;
+  static class Literal extends Expr {
+    final Object value;
 
     Literal(Object value) {
       this.value = value;
     }
 
     @Override
-    public <R> R accept(Visitor<R> visitor) {
+    <R> R accept(Visitor<R> visitor) {
       return visitor.visitLiteralExpr(this);
     }
 
   }
-  public static class Unary extends Expr {
-    public final Token operator;
-    public final Expr right;
+  static class Unary extends Expr {
+    final Token operator;
+    final Expr right;
 
     Unary(Token operator, Expr right) {
       this.operator = operator;
@@ -66,7 +66,7 @@ public abstract class Expr {
     }
 
     @Override
-    public <R> R accept(Visitor<R> visitor) {
+    <R> R accept(Visitor<R> visitor) {
       return visitor.visitUnaryExpr(this);
     }
 
